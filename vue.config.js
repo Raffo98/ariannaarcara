157b12240e5ee157f4bc550fa8fef5dddcb4fcdc
Raffaele Riccardelli <37075190+Raffo98@users.noreply.github.com>
2025-01-22 17:33:10 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,8 @@ const {defineConfig} = require('@vue/cli-service');
 
 module.exports = {
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/ariannaarcara/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/ariannaarcara/' : '/',
+  publicPath: '/ariannaarcara/',
   outputDir: "dist",
   configureWebpack: {
     resolve: {
