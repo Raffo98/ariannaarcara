cb22337a1e1d150986e437650616ba8e6a45f3c5
Raffaele Riccardelli <37075190+Raffo98@users.noreply.github.com>
2025-01-22 14:50:43 +0100

    d

--- src/plugins/airtable.js
+++ src/plugins/airtable.js
@@ -1,10 +1,33 @@
-var Airtable = require('airtable');
+// var Airtable = require('airtable');
+// require('dotenv').config();
+// const apiKey = process.env.API_KEY;
+// Airtable.configure({
+//     endpointUrl: 'https://api.airtable.com',
+//     apiKey: API_KEY
+//     // apiKey: 'pat7gis5M5WUjBB0k.96c185880f2a0373812348634a9edd77aac29b451fe97d52a2ad1730d1e8df1c'
+// });
+// var base = Airtable.base('appV4dTy0kqasy4Ch');
+
+// export default {
+//     base
+// }
+
+import Airtable from 'airtable';
+import 'dotenv/config';
+
+const apiKey = process.env.API_KEY;
+
+if (!apiKey) {
+  throw new Error("API_KEY non definita. Assicurati che sia presente nel file .env");
+}
+
 Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: 'pat7gis5M5WUjBB0k.96c185880f2a0373812348634a9edd77aac29b451fe97d52a2ad1730d1e8df1c'
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: apiKey,
 });
-var base = Airtable.base('appV4dTy0kqasy4Ch');
+
+const base = Airtable.base('appV4dTy0kqasy4Ch');
 
 export default {
-    base
-}
+  base,
+};
