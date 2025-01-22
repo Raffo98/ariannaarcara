ef4b82740067e58f6a954d33d7a5ef5ad9725d09
Raffaele Riccardelli <37075190+Raffo98@users.noreply.github.com>
2025-01-22 23:03:16 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -43,6 +43,7 @@ module.exports = {
         __VUE_I18N_LEGACY_API__: true,
         __INTLIFY_PROD_DEVTOOLS__: true,
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+        'process.env': JSON.stringify(process.env),
       }),
     ],
     optimization: {
